a0f0353c638cf8f2e53874d6b5e73e39d588fa96
Brandon Goldberg <brandonbng23@gmail.com>
2026-08-26 10:52:10 -0400

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import React from "react";
 import "./App.css";
 
 function App(): React.JSX.Element {
-    return (
-        <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        UD CISC275 with React Hooks and TypeScript
+      </header>
+      <h3>Brandon Goldberg</h3>
+      <p>
+        Edit <code>src/App.tsx</code> and save. This page will automatically
+        reload.
+      </p>
+    </div>
+  );
 }
 
 export default App;
