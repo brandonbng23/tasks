4416b3d0db29695048b84b78e33c981485c11482
Brandon Goldberg <brandonbng23@gmail.com>
2026-09-02 10:35:42 -0400

    Added styling, HTML elements, and assets to satisfy task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import "./App.css";
 
 function App(): React.JSX.Element {
@@ -7,12 +8,56 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <h3>Brandon Goldberg</h3>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-            <p>Hello World</p>
+            <h1>This is header text</h1>
+            <Container>
+                <Row>
+                    <Col>
+                        <p>Brandon Goldberg</p>
+                        <p>
+                            Edit <code>src/App.tsx</code> and save. This page
+                            will automatically reload.
+                        </p>
+                        <p>Hello World</p>
+                        <img
+                            src="./phin.jpeg"
+                            alt="Phineas, a medium-sized dog with white fur and a brown nose."
+                        ></img>
+                        <ul>
+                            <li>This</li>
+                            <li>is</li>
+                            <li>a</li>
+                            <li>list</li>
+                        </ul>
+
+                        <button
+                            type="button"
+                            className="btn btn-primary"
+                            onClick={() => {
+                                console.log("Hello World!");
+                            }}
+                        >
+                            Log Hello World
+                        </button>
+                        <div
+                            style={{
+                                backgroundColor: "red",
+                                height: "50px",
+                                width: "75px",
+                            }}
+                        ></div>
+                    </Col>
+
+                    <Col>
+                        <div
+                            style={{
+                                backgroundColor: "red",
+                                height: "50px",
+                                width: "75px",
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
