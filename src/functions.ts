a4dc52297ca0c091e8c8b281d1a61801506dec45
Brandon Goldberg <brandonbng23@gmail.com>
2026-09-02 22:46:03 -0400

    Added logic to complete convertYesNo function using toLowerCase() method and ternary

--- src/functions.ts
+++ src/functions.ts
@@ -43,5 +43,12 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    word = word.toLowerCase();
+    return (
+        word === "yes" || word === "no" ?
+            word === "yes" ?
+                true
+            :   false
+        :   null
+    );
 }
