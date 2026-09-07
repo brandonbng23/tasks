ee43223da8d1c019ba3f1b64d5030a229c47a8e3
Brandon Goldberg <brandonbng23@gmail.com>
2026-09-07 11:11:47 -0400

    Added logic to return a formatted string according to argued object's fields to complete toMarkdown() function

--- src/objects.ts
+++ src/objects.ts
@@ -75,7 +75,18 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    return (
+        "# " +
+        question.name +
+        "\n" +
+        question.body +
+        (question.type === "multiple_choice_question" ?
+            "\n" +
+            question.options
+                .map((option: string): string => "- " + option)
+                .join(`\n`)
+        :   "")
+    );
 }
 
 /**
