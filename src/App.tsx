efce0afaafcf7a1c495b02ff42a20a0d4d8194b8
Brandon Goldberg <brandonbng23@gmail.com>
2026-09-15 00:03:08 -0400

    Added preliminary solution for StartAttempt

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
-// import { StartAttempt } from "./components/StartAttempt";
+import { StartAttempt } from "./components/StartAttempt";
 // import { TwoDice } from "./components/TwoDice";
 // import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
@@ -17,7 +17,7 @@ function App(): React.JSX.Element {
             <hr />
             <RevealAnswer></RevealAnswer>
             <hr />
-            {/* <StartAttempt></StartAttempt> */}
+            <StartAttempt></StartAttempt>
             <hr />
             {/* <TwoDice></TwoDice> */}
             <hr />
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,38 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
-    return <div>Start Attempt</div>;
+    const [progress, setProgress] = useState<boolean>(false);
+    const [attempts, setAttempts] = useState<number>(4);
+
+    function startAttempt(): void {
+        setProgress(true);
+        setAttempts(attempts - 1);
+    }
+
+    function endAttempt(): void {
+        setProgress(false);
+    }
+
+    function mulligan(): void {
+        setAttempts(attempts + 1);
+    }
+
+    function disabled(): void {
+        return;
+    }
+
+    return (
+        <div>
+            <Button
+                onClick={!progress && attempts > 0 ? startAttempt : disabled}
+            >
+                Start Quiz
+            </Button>
+            <Button onClick={progress ? endAttempt : disabled}>
+                Stop Quiz
+            </Button>
+            <Button onClick={!progress ? mulligan : disabled}>Mulligan</Button>
+            <div>{attempts}</div>
+        </div>
+    );
 }
